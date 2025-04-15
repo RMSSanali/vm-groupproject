61f6fa4cd4b23286e9403f95a59cec0d63e2a60d
Patricio <Patte112@hotmail.com>
2025-04-15 14:02:05 +0200

    Add project files.

---
+++ ApiForGroup/Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiForGroup.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is a test API for the VM");
+        }
+    }
+}
